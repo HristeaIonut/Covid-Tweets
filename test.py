38a62ba06034f1f7519b4dc0e7879909d236beea
HristeaIonut <hristeaionut72@gmail.com>
2022-01-11 18:37:15 +0200

    Indeed good model and testing script

---
+++ test.py
@@ -0,0 +1,49 @@
+from keras.models import load_model
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import pickle as pkl
+import tweepy
+import numpy as np
+
+CONSUMER_KEY = 'Dv7PE2aL7XwwKguIQIjVVdTU1'
+CONSUMER_SECRET = '3vBKSLS2z2EeW8HmV83waJfaYTyAYNOi47lBaXScCcna8l8tws'
+
+def convert(max_words, text, max_length):
+    tokenizer = Tokenizer(num_words=max_words)
+    tokenizer.fit_on_texts(text)
+    sequences = tokenizer.texts_to_sequences(text)
+    tweet = pad_sequences(sequences, maxlen=max_length)
+    return tweet
+
+def predict_one(id):
+
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+    data = pkl.load(open("data_with_retweet.pkl", "rb"))
+    X, _ = zip(*data)
+    one_text = None
+    try:
+        tweet = api.get_status(id=id, tweet_mode='extended')
+        if tweet.retweeted_status:
+            one_text = tweet.retweeted_status.full_text
+        else:
+            one_text = tweet.full_text
+        print(one_text)
+    except:
+        print("error")
+
+    X = np.append(X, one_text)
+
+    X = convert(20000, X, 104)
+
+    model = load_model("model.h5")
+    res = model.predict(X)
+    print(np.round(res[-1]).astype(np.int32))
+
+
+
+predict_one(1465832997727449095)
+
+
+
+
--- train.py
+++ train.py
@@ -24,7 +24,6 @@ new_range = 1 - 0
 y = (y - (-1)) * new_range / old_range
 y = [0 if i < 0.5 else 1 for i in y]
 y = np.asarray(y, dtype=np.int32)
-# print(y[0:20])
 
 max_words = 20000
 tokenizer = Tokenizer(num_words=max_words)
@@ -33,16 +32,10 @@ sequences = tokenizer.texts_to_sequences(X)
 tweets = pad_sequences(sequences, maxlen=max_length)
 vocab_size = len(tokenizer.word_index)+1
 
-
-# model = Sequential()
-# model.add(Embedding(20000, 128))
-# model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
-# model.add(Dense(1, activation='sigmoid'))
-
-# model.compile(optimizer="adam", loss='mse', metrics=['accuracy'])
-# model.fit(tweets, y, batch_size=32, epochs=10)
-# model.save("model.h5")
-model = load_model("model.h5")
-res = model.predict(tweets)
-for i in range(20):
-    print(np.round(res[i]), y[i])
+model = Sequential()
+model.add(Embedding(20000, 128))
+model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer="adam", loss='mse', metrics=['accuracy'])
+model.fit(tweets, y, batch_size=32, epochs=10)
+model.save("model.h5")
