70c97823b6713a256d57b382e35522caf443fd8d
Radu Tache <radutache76@gmail.com>
2022-01-22 20:49:47 +0200

    95.72% accuracy

--- train.py
+++ train.py
@@ -13,11 +13,8 @@ if __name__ == '__main__':
     X, y = zip(*data)
     X = np.asarray(X)
     y = np.asarray(y, dtype=np.float32)
-    old_range = 1 - (-1)
-    new_range = 1 - 0
-    y = (y - (-1)) * new_range / old_range
-    y = [0 if i < 0.5 else 1 for i in y]
-    y = np.asarray(y, dtype=np.int32)
+    y = np.rint(y + 1)
+    y = np.asarray([[1 if i == z else 0 for i in range(3)] for z in y])
 
     embedding = pickle.load(open('models/embedding.layer', 'rb'))
 
@@ -31,12 +28,12 @@ if __name__ == '__main__':
         LSTM(128, dropout=0.2, recurrent_dropout=0.2),
         Dense(64, activation='relu'),
         Dense(32, activation='relu'),
-        Dense(1, activation='sigmoid')
+        Dense(3, activation='softmax')
     ])
 
     # model = CovidTweets('datas/metadata.tsv', 'datas/vectors.tsv')
-    model.compile(loss='mse',
+    model.compile(loss='categorical_crossentropy',
              optimizer='adam', metrics=['accuracy'])
 
-    model.fit(X, y, batch_size=128, epochs=20, validation_split=0.2)
-    model.save("model.h5")
+    model.fit(X, y, batch_size=128, epochs=10, validation_split=0.2)
+    model.save("models/model.h5")
